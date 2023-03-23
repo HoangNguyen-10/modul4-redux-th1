b5bf4ed44b0c5702dd67b82dd37f8fd5c389cdbc
HoangNguyen-10 <nguyenhoang91nd@gmail.com>
2023-03-23 10:20:47 +0700

    update

---
+++ src/redux/store.jsx
@@ -0,0 +1,14 @@
+import { applyMiddleware } from "redux";
+import rootReducer from "./reducer";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "../saga/userSaga";
+import { legacy_createStore as createStore } from 'redux'
+// Apply middleware
+const sagaMiddleware = createSagaMiddleware();
+// đăng kí reducer cho redux quản lí
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+// Chạy middleware cho redux để chạy các effect taị dòng code
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
